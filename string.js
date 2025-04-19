e264e53f28f50eaf07187627a161b5d60e8af63e
Ash0450 <malviya.ashish22@gmail.com>
2025-04-19 15:08:49 +0530

    Create string.js

---
+++ string.js
@@ -0,0 +1,36 @@
+Interview questions:
+
+Deep Copy and shallow copy
+How object.freeze work if we hve const why we want to use object.freeze
+State management
+Write syntax of deep copy
+Lexical scope
+Other way to describe object in javascript
+What is spread operator
+
+How to detect compulation in angular how it works
+What is onpush in angular
+Data binding
+ngfor in div tag write it
+Ngif
+What is view child
+Route guard
+How to handle multiple promises
+Auth guards
+Promises
+Forms in angular
+How to dependency injector
+What the features of angular you have used
+How to transfer the data between component
+
+
+Node
+What is middle ware in nodejs
+What is macro and micro state in nide
+How to handle error in node
+What is nodejs how it works
+Async and sync function in nodejs
+
+Coding question 
+Merge two array into 1
+input = [1,0,2,0,3] move all zero at the end
